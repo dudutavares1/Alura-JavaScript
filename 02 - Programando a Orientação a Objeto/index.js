eddf0a8eca53fe780b1b99a5e2b95f6b653bfaa2
Oficina Caçapava 350 <oficicacapava350@outlook.com>
2021-06-17 15:32:01 -0300

    atualizando a parte de saldo do cliente

--- 02 - Programando a Orientação a Objeto/index.js
+++ 02 - Programando a Orientação a Objeto/index.js
@@ -12,38 +12,30 @@ class ContaCorrente {
         this.saldo -= valor
         }
   }
+  depositar(valor){
+    if(valor > 0){
+      this.saldo += valor
+    }
+  }
 }
-
 
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
-
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-
-
-
 
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.saldo = 0
 contaCorrenteRicardo.agencia = 1001;
 
+contaCorrenteRicardo.depositar(100);
+
+contaCorrenteRicardo.sacar(50)
 
 
-
-
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.saldo = 100;
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.sacar(550)
-
-
-
-console.log(contaCorrenteRicardo.saldo);
-console.log(cliente1);
-console.log(cliente2);
+console.log(contaCorrenteRicardo);
