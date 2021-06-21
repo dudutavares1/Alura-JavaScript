982892f93a2ecde626466e365998bd28de6df13c
dudutavares <80931058+dudutavares@users.noreply.github.com>
2021-06-21 18:52:33 -0300

    O que aprendemos nessa aula:
    Diferenças entre null e undefined
    
    Aprofundando nos conceitos de referências a um espaço de memória
    
    Get e Set
    
    Melhor encapsulamento da classe
    
    Protegendo atributos sensíveis

--- 03 - Programando a Orientação a Objeto/ContaCorrente.js
+++ 03 - Programando a Orientação a Objeto/ContaCorrente.js
@@ -1,6 +1,26 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
     agencia;
+    _cliente;
     _saldo = 0;
+
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo (){
+        return this._saldo
+    }
+
+
 
     sacar(valor) {
         if (this._saldo >= valor) {
@@ -11,13 +31,13 @@ export class ContaCorrente {
     }
 
     depositar(valor) {
-        if(valor <= 0) {
+        if (valor <= 0) {
             return;
         }
         this._saldo += valor;
     }
 
-    transferir (valor, conta) {
+    transferir(valor, conta) {
         conta.cidade = 'Sao Paulo'
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
--- 03 - Programando a Orientação a Objeto/index.js
+++ 03 - Programando a Orientação a Objeto/index.js
@@ -6,10 +6,13 @@ const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
-const cliente2 = new Cliente();
 
+
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
+
+
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
@@ -17,7 +20,7 @@ contaCorrenteRicardo.cliente = cliente1;
 contaCorrenteRicardo.depositar(500);
 
 const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
+conta2.cliente = cliente1;
 conta2.agencia = 102;
 
 let valor = 200
