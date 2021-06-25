721f89136518927ebadc4ec8baa30b0c844399b5
Dudu Tavares <skynetdudu@icloud.com>
2021-06-25 13:49:08 -0300

    aula 3
    Manipulando Objetos DOM

---
+++ 08 - Programando Linguagem Web/js/main.js
@@ -0,0 +1,10 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+
+var peso = tdPesso.textContent
+
+console.log(paciente); // tr
+console.log(tdPeso); // td que tem o peso
