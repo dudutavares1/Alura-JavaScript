2eebe76659c725975657ee7eb1615e72459788e2
Dudu Tavares <skynetdudu@icloud.com>
2021-07-15 13:06:21 -0300

    aula 5
    boolean

---
+++ 03 - type-boolean.js
@@ -0,0 +1,11 @@
+const primeiroNumero = 5 
+const segundoNumero = 5
+console.log(primeiroNumero === segundoNumero)
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+const text1  = 'Alura'
+const text2 = 'alura'
+console.log(text1 === text2)
--- 09 - Fundamentos do JavaScript/dist/type-number.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// tipo number
-var meuNumero = 3;
-var primeiroNumero = 1;
-var segundoNumero = 2;
-var operacaoMatematica = primeiroNumero + segundoNumero;
-console.log(operacaoMatematica);
