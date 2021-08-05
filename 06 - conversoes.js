1c4cf47e2a50f517b1a7b0fd9ab5d0cb670310d3
Dudu Tavares <skynetdudu@icloud.com>
2021-08-05 16:54:33 -0300

    Create 06 - conversoes.js

---
+++ 06 - conversoes.js
@@ -0,0 +1,16 @@
+// tipo de dados de
+// boleanos 
+
+
+// conversao implicita 
+cont numero = 456
+const numeroString = '456'
+
+console.log(numero === numeroString)
+
+
+
+
+
+
+// conversao explicita  
